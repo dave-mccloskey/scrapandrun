e92dd2d65fe687a1784db20a4bb6313a8d9452ff
Dave McCloskey <davemccloskey@gmail.com>
2012-12-14 21:23:28 -0800

    add new stuff to admin

--- dsite/clothes/admin.py
+++ dsite/clothes/admin.py
@@ -1,4 +1,4 @@
-from clothes.models import Date, Outfit, Article, Color, ArticleType
+from clothes.models import Date, Outfit, Article, Color, ArticleType, AccessorizedOutfit, Store
 from django.contrib import admin
 
 
@@ -22,8 +22,18 @@ class ArticleTypeAdmin(admin.ModelAdmin):
     pass
 
 
+class AccessorizedOutfitAdmin(admin.ModelAdmin):
+    pass
+
+
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Date, ClothesAdmin)
 admin.site.register(Outfit, OutfitAdmin)
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Color, ColorAdmin)
 admin.site.register(ArticleType, ArticleTypeAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(AccessorizedOutfit, AccessorizedOutfitAdmin)
