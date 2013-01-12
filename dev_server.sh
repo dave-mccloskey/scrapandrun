36132e7c2d30996e5392c762c97832e11d3f884f
Dave McCloskey <davemccloskey@gmail.com>
2013-01-12 10:51:36 -0800

    run locally for dev server

--- dev_server.sh
+++ dev_server.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 dev_appserver.py src \
-  --mysql_user=clothes_dev \
-  --mysql_password=clothes_dev \
   --port=9000 \
-  --address=192.168.0.1 \
+  --address=localhost \
   -d
