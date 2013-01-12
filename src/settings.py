81da808dd4ec87a9f51ba02903fc7eb0798aa0a4
Dave McCloskey <davemccloskey@gmail.com>
2013-01-12 10:52:05 -0800

    connect to shady for dev server even from pc

--- src/settings.py
+++ src/settings.py
@@ -31,7 +31,7 @@ else:
             'NAME': 'clothes',  # Or path to database file if using sqlite3.
             'USER': 'clothes_dev',  # Not used with sqlite3.
             'PASSWORD': 'clothes_dev',  # Not used with sqlite3.
-            'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
+            'HOST': 'shady.akwire.lan',  # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',  # Set to empty string for default. Not used with sqlite3.
         }
     }
