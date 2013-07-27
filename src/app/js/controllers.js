d9c3cf4b6de4f9af92fce568cef665cd737421e2
Dave McCloskey <davemccloskey@gmail.com>
2013-07-27 00:11:13 -0700

    playing around with ang

--- src/app/js/controllers.js
+++ src/app/js/controllers.js
@@ -2,10 +2,16 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+function MyCtrl1($scope) {
+  $scope.x = 'test1';
+};
+MyCtrl1.$inject = ['$scope'];
 
-  }])
-  .controller('MyCtrl2', [function() {
+function MyCtrl2($scope) {
+  $scope.x = 'test2';
+};
+MyCtrl2.$inject = ['$scope'];
 
-  }]);
+angular.module('clothes.controllers', [])
+  .controller('MyCtrl1', [MyCtrl1])
+  .controller('MyCtrl2', [MyCtrl2]);
