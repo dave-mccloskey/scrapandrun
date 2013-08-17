2c2fdccd5df4e2c760e263355e660f89b92780de
Dave McCloskey <davemccloskey@gmail.com>
2013-08-17 11:02:09 -0700

    upgrade to django 1.5

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls.defaults import *
-from django.views.generic import DetailView, ListView
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from clothes.models import *
 
 urlpatterns = patterns('clothes.views',
-    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 )
--- src/settings.py
+++ src/settings.py
@@ -45,6 +45,7 @@ else:
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'America/Los_Angeles'
+USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
