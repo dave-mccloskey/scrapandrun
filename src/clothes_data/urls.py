005cedd0b05448fe511bceab2bf8d51f6de06159
Dave McCloskey <davemccloskey@gmail.com>
2013-08-17 13:55:06 -0700

    added some viewsets

--- src/clothes_data/urls.py
+++ src/clothes_data/urls.py
@@ -7,13 +7,29 @@ from rest_framework import viewsets, routers
 # ViewSets define the view behavior.
 
 
-class ColorViewSet(viewsets.ModelViewSet):
+class ColorViewSet(viewsets.ReadOnlyModelViewSet):
     model = Color
+
+
+class StoreViewSet(viewsets.ReadOnlyModelViewSet):
+    model = Store
+
+
+class ArticleTypeViewSet(viewsets.ReadOnlyModelViewSet):
+    model = ArticleType
+
+
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
+    model = Article
+    paginate_by = 10
 
 
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'colors', ColorViewSet)
+router.register(r'stores', StoreViewSet)
+router.register(r'articletypes', ArticleTypeViewSet)
+router.register(r'articles', ArticleViewSet)
 
 urlpatterns = patterns(
     'clothes2.views',
