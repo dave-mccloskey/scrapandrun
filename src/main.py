69d3dc6fb429fc9987cc536ddfd7fc39c7ca3bff
Dave McCloskey <davemccloskey@gmail.com>
2013-08-17 23:53:18 -0700

    add lib paths to path instead of using links

--- src/main.py
+++ src/main.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 # Include current directory in path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+pwd = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, pwd)
+
+lib_dir = os.path.join(pwd, 'lib')
+sys.path.insert(0, os.path.join(lib_dir, 'gdata-2.0.17/src'))
+sys.path.insert(0, os.path.join(lib_dir, 'lib/django-picasa-1.3'))
+sys.path.insert(0, os.path.join(lib_dir, 'lib/South-0.7.6'))
 
 # Define app
 app = django.core.handlers.wsgi.WSGIHandler()
