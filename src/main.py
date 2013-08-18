87926dcf84500dbc17984470b82fff090ac1ed47
Dave McCloskey <davemccloskey@gmail.com>
2013-08-18 00:01:34 -0700

    move libs to a flat lib dir

--- src/main.py
+++ src/main.py
@@ -4,12 +4,8 @@ import sys
 
 # Include current directory in path
 pwd = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, pwd)
-
 lib_dir = os.path.join(pwd, 'lib')
-sys.path.insert(0, os.path.join(lib_dir, 'gdata-2.0.17/src'))
-sys.path.insert(0, os.path.join(lib_dir, 'lib/django-picasa-1.3'))
-sys.path.insert(0, os.path.join(lib_dir, 'lib/South-0.7.6'))
+sys.path.extend([pwd, lib_dir])
 
 # Define app
 app = django.core.handlers.wsgi.WSGIHandler()
