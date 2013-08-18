649fe302cc47e3524074706d727096c3fb221ab4
Dave McCloskey <davemccloskey@gmail.com>
2013-08-17 18:58:57 -0700

    include image of first outfit for day

--- src/app/js/controllers.js
+++ src/app/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('clothes.controllers', ['ngResource'])
     .controller('Dashboard', function($scope, $resource) {
-      this.Date = $resource('/_/clothes_data/dates', {page_size: 4});
+      this.Date = $resource('/_/clothes_data/dates/', {page_size: 4});
       $scope.dates = this.Date.get();
     })
     .controller('MyCtrl2', function($scope) {
--- src/clothes_data/serializers.py
+++ src/clothes_data/serializers.py
@@ -3,11 +3,19 @@ from clothes.models import *
 
 
 class DateSerializer(serializers.HyperlinkedModelSerializer):
-
-    """
-    Serializer for Dates.
-    """
+    photo = serializers.Field(source='first_outfit_photo.src')
 
     class Meta:
         model = Date
+        fields = ('date', 'outfits_worn', 'photo')
+
+
+class AccessorizedOutfitSerializer(serializers.HyperlinkedModelSerializer):
+
+    """
+    Serializer for AccessorizedOutfits.
+    """
+
+    class Meta:
+        model = AccessorizedOutfit
         fields = ('date', 'outfits_worn')
