c61bf6fe3456a79282aced1b59cd0fc9cf8e9a97
Dave McCloskey <davemccloskey@gmail.com>
2013-08-17 22:11:13 -0700

    pretty date dashboard

--- src/clothes_data/serializers.py
+++ src/clothes_data/serializers.py
@@ -3,11 +3,14 @@ from clothes.models import *
 
 
 class DateSerializer(serializers.HyperlinkedModelSerializer):
-    photo = serializers.Field(source='first_outfit_photo.src')
+    photo = serializers.SerializerMethodField('get_photo_url')
 
     class Meta:
         model = Date
         fields = ('date', 'outfits_worn', 'photo')
+
+    def get_photo_url(self, obj):
+        return obj.first_outfit_photo().src(700)
 
 
 class AccessorizedOutfitSerializer(serializers.HyperlinkedModelSerializer):
