74d206015ac8aca1d76519d0c8e7cdabb29a03b3
Dave McCloskey <davemccloskey@gmail.com>
2013-09-01 22:35:11 -0700

    calendar should now display all data for the month (no moving between months yet)

--- src/app/js/controllers.js
+++ src/app/js/controllers.js
@@ -3,6 +3,49 @@ angular.module('clothes.controllers', ['ngResource'])
       this.Date = $resource('/_/clothes_data/dates/', {page_size: 4});
       $scope.dates = this.Date.get();
     })
-    .controller('Calendar', function($scope, $resource) {
+    .controller('Calendar', function($scope, $resource, $location) {
+      var yearmonth = moment().format('YYYYMM');
+      this.Date = $resource('/_/clothes_data/dates/', {yearmonth: yearmonth});
+      var datesWithData = this.Date.get(function() {
+        var firstday = moment().startOf('month').startOf('week');
+        var lastday = moment().endOf('month').endOf('week');
 
+        // dates is a hash from unix timestamp to { date: foo }
+        var dates = {};
+        var d = firstday;
+        while (d.isBefore(lastday)) {
+          dates[d.valueOf()] = {date: d.valueOf()};
+          d.add('days', 1);
+        }
+        // update dates to use value from request if data available
+        $.each(datesWithData.results, function(index, date) {
+          d = moment(date.date, 'YYYY-MM-DD').valueOf();
+          dates[d] = date;
+          date['date'] = d;
+        });
+
+        // extract values of object into an array
+        var dateArray = [];
+        $.each(dates, function(key, value) {
+          dateArray.push(value);
+        });
+        $scope.dates = dateArray;
+
+        // array containing week numbers based on number of days
+        var weeks = [];
+        for (var week = 0; week * 7 < dateArray.length; week++) {
+          weeks.push(week);
+        }
+        $scope.weeks = weeks;
+      });
+    })
+    .filter('week', function() {
+      return function(array, week) {
+        if (!array) {
+          return array;
+        }
+        var begin = week * 7;
+        var end = begin + 7;
+        return array.slice(begin, end);
+      };
     });
