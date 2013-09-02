831794c2acea3cece8288b89833e2a2f49214d4d
Dave McCloskey <davemccloskey@gmail.com>
2013-09-01 20:43:57 -0700

    allow filtering dates by year/month in api

--- src/clothes_data/serializers.py
+++ src/clothes_data/serializers.py
@@ -10,7 +10,8 @@ class DateSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('date', 'outfits_worn', 'photo')
 
     def get_photo_url(self, obj):
-        return obj.first_outfit_photo().src(700)
+        photo = obj.first_outfit_photo()
+        return photo.src(700) if photo else ""
 
 
 class AccessorizedOutfitSerializer(serializers.HyperlinkedModelSerializer):
