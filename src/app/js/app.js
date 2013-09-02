70012510a42211e07447ccf2bd6b31f4cef73c40
Dave McCloskey <davemccloskey@gmail.com>
2013-09-01 17:35:49 -0700

    add nav for calendar along with highlighting dynamically

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,11 +1,12 @@
 angular.module('clothes', ['clothes.controllers',
           'clothes.filters',
-          'clothes.services'
+          'clothes.services',
+          'link'
     ])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/dashboard',
             {templateUrl: 'partials/dashboard.html', controller: 'Dashboard'});
-        $routeProvider.when('/view2',
-            {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.when('/calendar',
+            {templateUrl: 'partials/calendar.html', controller: 'Calendar'});
         $routeProvider.otherwise({redirectTo: '/dashboard'});
       }]);
--- src/app/js/directives.js
+++ src/app/js/directives.js
@@ -1,3 +1,20 @@
-'use strict';
-
-/* Directives */
+angular.module('link', [])
+    .directive('activeLink', ['$location', function(location) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs, controller) {
+          var clazz = attrs.activeLink;
+          var path = $(element).children("a")[0].hash.substring(1);
+          console.log('path for ' + element + ' is ' + path);
+          scope.location = location;
+          scope.$watch('location.path()', function(newPath) {
+            console.log('checking ' + path + ' and ' + newPath);
+            if (path === newPath) {
+              element.addClass(clazz);
+            } else {
+              element.removeClass(clazz);
+            }
+          });
+        }
+      }
+    }]);
